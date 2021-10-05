3c0cc83a2a2d1ebfc9568d51d2f6ef9e6bd1c8e3
Abhishek <rai.abhi.1696@gmail.com>
2021-10-05 11:46:28 +0530

    Greetings bug fixed!
    A semicolon was missed and it was not allowing the file to build.

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,6 +4,6 @@ int main(int argc, char const *argv[])
     int user = 777;
     printf("Hello World, I am %d\n",user);
     printf("A vectorpie\n");
-    printf("A conglomerate of Stardust!!")
+    printf("A conglomerate of Stardust!!");
     return 0;
 }
