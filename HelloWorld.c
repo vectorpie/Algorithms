1033a4075986a48ed90201ff655897d042df120d
vectorpie <rai.abhi.1696@gmail.com>
2021-10-28 20:01:37 +0530

    Added code to read from Command line

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,6 +4,11 @@ int main(int argc, char const *argv[])
     int user = 777;
     printf("Hello World, I am %d\n",user);
     printf("A vectorpie\n");
-    printf("A conglomerate of Stardust!!");
+    printf("A conglomerate of Stardust!!\n");
+    printf("Number of character from comand-line - %d\n",argc);
+    printf("Strings from command-line ...\n");
+    for(int i=0;i<argc;i++){
+        printf("%s ",argv[i]);
+    }
     return 0;
 }
